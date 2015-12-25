417c5d61261269b080eb36352eddca3e9b3ea8d3
Rudy Reeves <rudyreeves@lclark.edu>
2015-12-25 02:16:15 -0500

    Completed Lesson 5

--- Lesson_5/00_Conference_Central/models.py
+++ Lesson_5/00_Conference_Central/models.py
@@ -17,6 +17,10 @@ import endpoints
 from protorpc import messages
 from google.appengine.ext import ndb
 
+class StringMessage(messages.Message):
+    """StringMessage-- outbound (single) string message"""
+    data = messages.StringField(1, required=True)
+
 class ConflictException(endpoints.ServiceException):
     """ConflictException -- exception mapped to HTTP 409 response"""
     http_status = httplib.CONFLICT
